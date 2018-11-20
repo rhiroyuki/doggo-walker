988235cf2f4bb1b91f12cd532cb68294c9567563
Ricardo Eihara <ricardo.hejr@gmail.com>
2018-11-20 02:21:42 -0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do |i|
+  owner = PetOwner.create(name: "PetOwner#{i}")
+  Pet.create(name: "Doggo #{i}", pet_owner: owner, breed: 'Chihuahua')
+  Pet.create(name: "Dogginho #{i}", pet_owner: owner, breed: 'Yorkshire')
+  Pet.create(name: "Doggão #{i}", pet_owner: owner, breed: 'Akita')
+end
+
+pets = Pet.where(breed: 'Akita')
+DogWalking.create(
+  pets: pets,
+  price_value: 150.0,
+  scheduled_duration: 30,
+  scheduled_on: Time.now + 1.day)
+
+pets = Pet.where(breed: 'Yorkshire')
+DogWalking.create(
+  pets: pets,
+  price_value: 90.0,
+  scheduled_duration: 60,
+  scheduled_on: Time.now + 1.day)
+
+ended_at = Time.now - 1.day - 30.minutes
+pets = Pet.where(breed: 'Chihuahua')
+DogWalking.create(
+  pets: pets,
+  price_value: 60.0,
+  scheduled_duration: 30,
+  scheduled_on: Time.now - 1.day,
+  started_at: Time.now - 1.day,
+  ended_at: ended_at)
+
