4c6963851a924d593045635402288f5b8407f3c5
Ricardo Eihara <ricardo.hejr@gmail.com>
2018-11-19 23:23:50 -0200

    add factory bot

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ end
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.8'
+  gem 'factory_bot_rails'
 end
 
 group :test do
---
+++ spec/factories/dog_walking_factory.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :dog_walking do
+    price_value { 10.0 }
+  end
+end
